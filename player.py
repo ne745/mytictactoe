83c2b208ff1281b63f32dac4936a1ba092b87c79
ne745 <kkpma0@gmail.com>
2022-08-20 23:34:07 +0900

    抽象クラスを実装

--- player.py
+++ player.py
@@ -1,7 +1,20 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 
 
-class RandomPlayer:
+class Player(ABC):
+
+    def __init__(self, player=None):
+        self.player = player
+
+    @abstractmethod
+    def play(self, board):
+        pass
+
+
+class RandomPlayer(Player):
+
     def play(self, board):
         moves = board.valid_moves()
         idx = np.random.choice(moves)
