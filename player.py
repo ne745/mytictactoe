d303101035e644de0fc2f436065949c977adc064
ne745 <kkpma0@gmail.com>
2022-08-20 23:38:39 +0900

    ミニマックスアルゴリズムを実装したプレイヤーを追加

--- player.py
+++ player.py
@@ -37,3 +37,61 @@ class BetterPlayer(Player):
         idx = np.random.choice(moves)
         print('少し賢いプレイヤー: ', idx)
         board.move(idx)
+
+
+class BestPlayer(Player):
+
+    def play(self, board):
+        score, idx = self._minimax(board, self.player)
+        print('最強のAI: ', idx)
+        board.move(idx)
+
+    def _minimax(self, board, player):
+        """
+        ミニマックスアルゴリズム
+
+        Args:
+            board(list): 盤面のリスト
+            player(int): プレイヤー
+
+        Returns:
+            tuple: (スコア, 選択する盤面)
+        """
+
+        maximize_player = 0
+        minimize_player = 1
+
+        if board.is_win(maximize_player):
+            return (1, None)
+        elif board.is_win(minimize_player):
+            return (-1, None)
+        elif board.is_end():
+            return (0, None)
+
+        opp = 1 if player == 0 else 0
+        if player == maximize_player:
+            max_score = -np.inf
+            max_idx = None
+
+            for idx in board.valid_moves():
+                board.move(idx)
+                score, next_idx = self._minimax(board, opp)
+                if max_score < score:
+                    max_score = score
+                    max_idx = idx
+                board.unmove(idx)
+
+            return (max_score, max_idx)
+        else:
+            min_score = np.inf
+            min_idx = None
+
+            for idx in board.valid_moves():
+                board.move(idx)
+                score, next_idx = self._minimax(board, opp)
+                if min_score > score:
+                    min_score = score
+                    min_idx = idx
+                board.unmove(idx)
+
+            return (min_score, min_idx)
--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-from player import RandomPlayer, BetterPlayer
+from player import RandomPlayer, BetterPlayer, BestPlayer
 
 
 MARKS = {0: 'X', 1: 'O'}
@@ -61,7 +61,7 @@ class Board:
 
 def main():
     board = Board()
-    players = [RandomPlayer(), BetterPlayer(1)]
+    players = [RandomPlayer(), BestPlayer(1)]
     player = 0  # 0 or 1
 
     while True:
